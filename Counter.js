01a9e9c6c4cd97b08c9edcc3789421f0f980754e
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-01 15:08:34 -0700

    convert state to object from number

--- Counter.js
+++ Counter.js
@@ -3,15 +3,17 @@ const React = require('react');
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = 0;
+    this.state = {
+      count: 0,
+    };
     this.increment = (event) => {
       console.log('run');
-      this.setState(prevState => prevState + 1);
+      this.setState(prevState => ({ count: prevState.count + 1 }));
     };
   }
 
   render() {
-    const h1 = React.createElement('h1', { key: 1 }, this.state);
+    const h1 = React.createElement('h1', { key: 1 }, this.state.count);
     const inc = React.createElement('button', { type: 'button', onClick: this.increment, key: 2 }, '+');
     return React.createElement('div', null, [h1, inc]);
   }
